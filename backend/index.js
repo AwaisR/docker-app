13cbd96e85527b9fa3f6b2f0f012489f97bbaeb1
AwaisR <awaisrazzaa@gmail.com>
2023-01-10 13:20:14 -0600

    add docker application

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose =require("mongoose")
+const app = express();
+const path = require('path');
+app.use('/welcome',(req,res)=>{
+    res.send("welcome")
+}),
+
+// database connection
+mongoose.connect("mongodb://localhost:27017/testing-db",()=>{
+console.log("connection successfull......")});
+
+app.listen(9000,()=>{
+    console.log('App is running on 8000')
+})
